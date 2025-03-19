16b8e3d82a1fb2baf38d28f26ca1d9beabca8b8b
mierlush <mirelamaletchi0409@gmail.com>
2025-03-19 10:39:21 +0200

    added galleries

--- js/script.js
+++ js/script.js
@@ -1,24 +1,25 @@
+function startSlideshow() {
+    const galleries = document.querySelectorAll('.gallery'); // Select all gallery sections
 
-var modal = document.getElementById("imageModal");
-var modalImage = document.getElementById("modalImage");
+    galleries.forEach(gallery => {
+        const galleryImages = gallery.querySelectorAll('img'); // Get images in this gallery
+        let currentIndex = 0;
 
-var images = document.querySelectorAll(".image-clickable");
+        // Hide all images initially in this gallery
+        galleryImages.forEach(img => img.style.opacity = 0);
+        // Show the first image in this gallery
+        galleryImages[currentIndex].style.opacity = 1;
 
-images.forEach(function(image) {
-    image.onclick = function() {
-        modal.style.display = "flex"; 
-        modalImage.src = this.src; 
-    }
-});
-
-
-var closeButton = document.querySelector(".close");
-closeButton.onclick = function() {
-    modal.style.display = "none"; 
+        // Set up an interval for this gallery
+        setInterval(() => {
+            // Hide the current image
+            galleryImages[currentIndex].style.opacity = 0;
+            // Move to the next image
+            currentIndex = (currentIndex + 1) % galleryImages.length;
+            // Show the new image
+            galleryImages[currentIndex].style.opacity = 1;
+        }, 5000);
+    });
 }
 
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-}
+window.addEventListener('load', startSlideshow);
